9cf59a8a9ee2029c6977031a7744e627357353ef
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-18 22:32:06 -0700

    Created index.html and script.js
    Created index.html and script.js

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+// Exercise 3 page 94
+"use strict";
+
+let employees = [
+    {
+    id: "55007",
+    name: "Karina Chambers",
+    jobTitle: "Tech Lead",
+    yearsAtCompany: 14,
+    email: "karinachambers@ziore.com",
+    wfhAdress: "640 Boynton Place, Faxon, Kentucky, 42071",
+    skillSet: "Velit commodo voluptate id est. Fugiat magna enim quis exercitation duis fugiat non nisi consequat.",
+    projectsAssignedTo: [
+        {
+            projectId: 112,
+            name: "Cupidatat aute"
+        }
+    ]},
+    {
+    id: "23810",
+    name: "Kasey Bowers",
+    jobTitle: "Senior Programmer",
+    yearsAtCompany: 19,
+    email: "kaseybowers@ziore.com",
+    wfhAdress: "969 Clarendon Road, Marshall, Vermont, 47859",
+    skillSet: "Est et voluptate incididunt deserunt culpa excepteur.",
+    projectsAssignedTo: [
+        {
+            projectId: 124,
+            name: "Amet do deserunttate aliqua"
+        }
+    ]},
+];
+
+window.onload = () => {
+    
+}
